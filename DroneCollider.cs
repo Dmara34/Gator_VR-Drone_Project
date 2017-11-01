e07f1b4235ac69de0a5e760d347cd3bdc679c6da
Dmara34 <32003696+Dmara34@users.noreply.github.com>
2017-11-01 10:29:59 -0400

    Add files via upload
    added scripts
           DroneCollider for changing the color of the capsule when triggered by an object
           Screenshot to create a photo when space is pressed
    added trigger colliders and a mesh collider to the drone object
    In the scene changes
            the Plane is now a convex collider.

---
+++ DroneCollider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneCollider: MonoBehaviour {
+public Color Safe_Radius_1_Color;
+public Color Safe_Radius_2_Color;
+public Color Safe_Radius_3_Color;
+public Color Safe_Radius_Default;
+	public GameObject capsule;
+	public Color Current_Capsule_Color;
+	void Start(){
+		//Capsule = RC_Quadcopter_FBX.Find("Capsule").GameObject;
+	capsule.GetComponent<Renderer> ().material.color		 = Safe_Radius_Default;	}
+//changes the color of the capsule
+void	OnTriggerEnter(Collider other){
+		if (capsule.GetComponent<Renderer> ().material.color == Safe_Radius_Default) {
+			capsule.GetComponent<Renderer> ().material.color = Safe_Radius_3_Color;
+			return;
+	}
+		if(capsule.GetComponent<Renderer> ().material.color ==Safe_Radius_3_Color){
+			capsule.GetComponent<Renderer> ().material.color = Safe_Radius_2_Color;
+			return;
+	}
+		if(capsule.GetComponent<Renderer> ().material.color ==Safe_Radius_2_Color){
+			capsule.GetComponent<Renderer> ().material.color = Safe_Radius_1_Color;
+			return;
+	}
+	
+}
+	void	OnTriggerExit(Collider other){
+		if (capsule.GetComponent<Renderer> ().material.color == Safe_Radius_1_Color) {
+			capsule.GetComponent<Renderer> ().material.color = Safe_Radius_2_Color;
+			return;
+		}
+		if(capsule.GetComponent<Renderer> ().material.color ==Safe_Radius_2_Color){
+			capsule.GetComponent<Renderer> ().material.color = Safe_Radius_3_Color;
+			return;
+		}
+		if(capsule.GetComponent<Renderer> ().material.color ==Safe_Radius_3_Color){
+			capsule.GetComponent<Renderer> ().material.color = Safe_Radius_Default;
+			return;
+		}
+	}
+}
