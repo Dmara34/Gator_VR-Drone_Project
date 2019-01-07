e5624a05200ad9ef7440b0ca1fb8913dfddb9b19
Dmara34 <32003696+Dmara34@users.noreply.github.com>
2019-01-06 21:59:17 -0500

    Add files via upload

--- Assets/Scripts/DroneMovementScript.cs
+++ Assets/Scripts/DroneMovementScript.cs
@@ -24,54 +24,24 @@ public class DroneMovementScript : MonoBehaviour {
 
 	public float upForce;
 	public float Speed;
+
 	void MovementUpDown(){
-		
-		Speed = ourDrone.velocity.magnitude;
-
-		if ((Mathf.Abs (Input.GetAxis ("Vertical")) > 0.2f || Mathf.Abs (Input.GetAxis ("Horizontal")) > 0.2f)) {
-			if (Input.GetKey (KeyCode.I) || Input.GetKey (KeyCode.K)) {
-				ourDrone.velocity = ourDrone.velocity;
-			}
-			if (!Input.GetKey (KeyCode.I) && !Input.GetKey (KeyCode.K) && !Input.GetKey (KeyCode.J) && !Input.GetKey (KeyCode.L)) {
-				ourDrone.velocity = new Vector3(ourDrone.velocity.x, Mathf.Lerp(ourDrone.velocity.y, 0, Time.deltaTime * 5), ourDrone.velocity.z);
-				upForce = 16;
-			}
-			if (!Input.GetKey (KeyCode.I) && !Input.GetKey (KeyCode.K) && (Input.GetKey (KeyCode.J) || Input.GetKey (KeyCode.L))) {
-				ourDrone.velocity = new Vector3(ourDrone.velocity.x, Mathf.Lerp(ourDrone.velocity.y, 0, Time.deltaTime * 5), ourDrone.velocity.z);
-				upForce = 16;
-			}
-			if(Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.L)){
-				upForce = 16;
-			}
-		}
-
-		if(Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f){
-			upForce = 26;
-		}
 		// Set keyboard inputs for drone to lift-off and go down
-		if (Input.GetKey (KeyCode.I)){
-			upForce = 30;
-				if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f){
-					upForce = 30;	
-				}
+		if (Input.GetAxis ("VerticalR") < 0){
+			upForce = 20f*Input.GetAxis ("VerticalR");
 		} 
-		else if (Input.GetKey (KeyCode.K)){
-			upForce = -26;
+		else if (Input.GetAxis ("VerticalR") > 0){
+			upForce = 20f*Input.GetAxis ("VerticalR");
 		} 
-		else if (!Input.GetKey (KeyCode.I) && !Input.GetKey (KeyCode.K) && (Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f)){
-			upForce = 9.81f;
-		}
-
 	}
-
 
 	private float movementForwardSpeed = 16.0f;
 	private float titlAmountForward = 0;
-	private float titlVelocityForward; //unnecesary
+
 	void MovementForward(){
-		if (Input.GetAxis ("Vertical") != 0) {
-			ourDrone.AddRelativeForce (Vector3.forward * Input.GetAxis ("Vertical") * movementForwardSpeed);
-			titlAmountForward = Mathf.SmoothDamp (titlAmountForward, 20 * Input.GetAxis ("Vertical"), ref titlVelocityForward, 0.1f);
+		if (Input.GetAxis ("VerticalL") != 0) {
+			ourDrone.AddRelativeForce (Vector3.forward * Input.GetAxis ("VerticalL") * movementForwardSpeed);
+			titlAmountForward = Mathf.SmoothDamp (titlAmountForward, 20 * Input.GetAxis ("VerticalL"), ref titlVelocityForward, 0.1f);
 
 		}
 	}
@@ -80,28 +50,26 @@ public class DroneMovementScript : MonoBehaviour {
 	private float rotateAmountByKeys = 2.5f;
 	private float rotationYVelocity;
 	void Rotation(){
-		if (Input.GetKey (KeyCode.J)) {
-			wantedYRotation -= rotateAmountByKeys;
-		}
-		if(Input.GetKey(KeyCode.L)){
-			wantedYRotation += rotateAmountByKeys;
-		}
+		if (Input.GetAxis ("HorizontalR")!=0) {
+			wantedYRotation += (Input.GetAxis("HorizontalR")*rotateAmountByKeys);
 
+
+		}
 		currentYRotation = Mathf.SmoothDamp (currentYRotation, wantedYRotation, ref rotationYVelocity, 0.25f);
 	}
 
 	private Vector3 velocityToSmoothDampToZero;
 	void ClampingSpeedValues(){
-		if (Mathf.Abs (Input.GetAxis ("Vertical")) > 0.2f && Mathf.Abs (Input.GetAxis ("Horizontal")) > 0.2f) {
+		if (Mathf.Abs (Input.GetAxis ("VerticalL")) > 0.2f && Mathf.Abs (Input.GetAxis ("HorizontalL")) > 0.2f) {
 			ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 10.0f, Time.deltaTime * 5f));
 		}
-		if (Mathf.Abs (Input.GetAxis ("Vertical")) > 0.2f && Mathf.Abs (Input.GetAxis ("Horizontal")) < 0.2f) {
+		if (Mathf.Abs (Input.GetAxis ("VerticalL")) > 0.2f && Mathf.Abs (Input.GetAxis ("HorizontalL")) < 0.2f) {
 			ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 10.0f, Time.deltaTime * 5f));
 		}
-		if (Mathf.Abs (Input.GetAxis ("Vertical")) < 0.2f && Mathf.Abs (Input.GetAxis ("Horizontal")) > 0.2f) {
+		if (Mathf.Abs (Input.GetAxis ("VerticalL")) < 0.2f && Mathf.Abs (Input.GetAxis ("HorizontalL")) > 0.2f) {
 			ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 10.0f, Time.deltaTime * 5f));
 		}
-		if (Mathf.Abs (Input.GetAxis ("Vertical")) < 0.2f && Mathf.Abs (Input.GetAxis ("Horizontal")) < 0.2f) {
+		if (Mathf.Abs (Input.GetAxis ("VerticalL")) < 0.2f && Mathf.Abs (Input.GetAxis ("HorizontalL")) < 0.2f) {
 			ourDrone.velocity = Vector3.SmoothDamp(ourDrone.velocity, Vector3.zero, ref velocityToSmoothDampToZero, 0.95f);
 		}
 	}
@@ -110,9 +78,9 @@ public class DroneMovementScript : MonoBehaviour {
 	private float tiltAmountSideways;
 	private float tiltAmountVelocity;
 	void Swirl(){
-		if (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0.2f) {
-			ourDrone.AddRelativeForce (Vector3.right * Input.GetAxis ("Horizontal") * sideMovementAmount);
-			tiltAmountSideways = Mathf.SmoothDamp (tiltAmountSideways, -20 * Input.GetAxis ("Horizontal"), ref tiltAmountVelocity, 0.1f);
+		if (Mathf.Abs (Input.GetAxis ("HorizontalL")) > 0.2f) {
+			ourDrone.AddRelativeForce (Vector3.right * Input.GetAxis ("HorizontalL") * sideMovementAmount);
+			tiltAmountSideways = Mathf.SmoothDamp (tiltAmountSideways, -20 * Input.GetAxis ("HorizontalL"), ref tiltAmountVelocity, 0.1f);
 		} 
 		else {
 			tiltAmountSideways = Mathf.SmoothDamp (tiltAmountSideways, 0, ref tiltAmountVelocity, 0.1f);
---
+++ Assets/Scripts/Screenshot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Screenshot : MonoBehaviour {
+	private int count;
+	public RawImage photo;
+	ArrayList photos = new ArrayList();
+	public Texture photo1;
+//save screenshots to folder
+//can be used to practice drone camera skills
+void LateUpdate() {
+		if (Input.GetAxis ("Camera")!=0) {
+			Debug.Log ("Screenshot captured");
+			if (count == 0) {
+				ScreenCapture.CaptureScreenshot (Application.dataPath+"/Photos/Photo.png");
+			} 
+			else {
+				ScreenCapture.CaptureScreenshot (Application.dataPath+"/Photos/Photo"+"("+count+")"+".png");
+			}
+			count++;
+		}
+	}
+}
