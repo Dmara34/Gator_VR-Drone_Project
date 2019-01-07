5368e1114f92bef94115cabb07a2f5e52ed1b42e
Dmara34 <32003696+Dmara34@users.noreply.github.com>
2019-01-06 22:00:22 -0500

    Add files via upload

---
+++ Assets/CrashBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class CrashBehavior : MonoBehaviour {
+GameObject[] CrashObjects;
+
+	void Start(){
+		//hide the crash screen
+		CrashObjects = GameObject.FindGameObjectsWithTag ("Crash");
+		hideCrash();
+	}
+	void	OnTriggerEnter(Collider other){
+		//pause game time and show crash screen
+		Time.timeScale = 0;
+		showCrash ();
+	}
+	public void showCrash(){
+		foreach(GameObject g in CrashObjects){
+			g.SetActive(true);
+		}
+	}
+	//game reset is required to hide crash screen
+	public void hideCrash(){
+		foreach(GameObject g in CrashObjects){
+			g.SetActive(false);
+		}
+	}
+}
